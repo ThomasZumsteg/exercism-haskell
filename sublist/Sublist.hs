52551ee611c666017b293edf8422df4fbdf1572e
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-06-25 11:18:04 -0700

    Second iteration sublist, now using Data.List.isInfixOf

--- sublist/Sublist.hs
+++ sublist/Sublist.hs
@@ -1,17 +1,11 @@
 module Sublist (Sublist(..), sublist) where
-  data Sublist = Equal | Sublist | Superlist | Unequal deriving (Show, Eq)
+import Data.List (isInfixOf)
 
-  sublist::(Eq a) => [a] -> [a] -> Sublist
-  sublist xs ys
-    | xs == ys = Equal
-    | xs `inside` ys = Sublist
-    | ys `inside` xs = Superlist
-    | otherwise = Unequal
+data Sublist = Equal | Sublist | Superlist | Unequal deriving (Show, Eq)
 
-  inside::(Eq a) => [a] -> [a] -> Bool
-  xs `inside` ys
-    | len_xs > len_ys = False
-    | xs == take len_xs ys = True
-    | otherwise = xs `inside` tail ys
-    where len_xs = length xs
-          len_ys = length ys
+sublist :: (Eq a) => [a] -> [a] -> Sublist
+sublist xs ys
+  | xs == ys = Equal
+  | xs `isInfixOf` ys = Sublist
+  | ys `isInfixOf` xs = Superlist
+  | otherwise = Unequal
