0dccaabe897bf7ef99199ba22d3192522c46596e
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-06-30 10:03:32 -0700

    Simplified bob

--- bob/bob.hs
+++ bob/bob.hs
@@ -4,17 +4,7 @@ import Data.Char (isLower, isSpace, isUpper)
 
 responseFor :: String -> String
 responseFor what
-  | nothing what  = "Fine. Be that way!" 
-  | shouting what = "Whoa, chill out!"
-  | question what = "Sure."
+  | all isSpace what = "Fine. Be that way!"
+  | any isUpper what && not(any isLower what) = "Whoa, chill out!"
+  | last what == '?' = "Sure."
   | otherwise = "Whatever."
-
-  where
-    nothing w = all isSpace w
-    question w
-      | isSpace $ last w = question $ init w
-      | otherwise = last w == '?'
-    shouting w = hasUppercase w && not(hasLowercase w)
-      where
-        hasUppercase = not . null . filter isUpper
-        hasLowercase = any isLower
