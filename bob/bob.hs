cb5571fd6790f0e33b0e66486c1562d565b4db19
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-01 10:17:13 -0700

    Added space to bob

--- bob/bob.hs
+++ bob/bob.hs
@@ -5,6 +5,6 @@ import Data.Char (isLower, isSpace, isUpper)
 responseFor :: String -> String
 responseFor what
   | all isSpace what = "Fine. Be that way!"
-  | any isUpper what && not(any isLower what) = "Whoa, chill out!"
+  | any isUpper what && not (any isLower what) = "Whoa, chill out!"
   | last what == '?' = "Sure."
   | otherwise = "Whatever."
