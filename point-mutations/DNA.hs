b039473f36741dc165bcda9b4ed8df318c929096
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-02 11:16:52 -0700

    point-mutations to use if/then/else

--- point-mutations/DNA.hs
+++ point-mutations/DNA.hs
@@ -2,4 +2,4 @@ module DNA (hammingDistance) where
 
 hammingDistance :: (Eq a) => [a] -> [a] -> Int
 hammingDistance xs ys = sum $ zipWith equal xs ys
-  where equal x y | x == y = 0 | otherwise = 1
+  where equal x y = if x == y then 0 else 1
