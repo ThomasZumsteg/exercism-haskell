c218268890eff00b73c98cec8ed1bd7568ff9680
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-06 13:45:02 -0700

    First iteration simple-linked-list

---
+++ simple-linked-list/LinkedList.hs
@@ -0,0 +1,27 @@
+module LinkedList where
+
+data Link a = Nil | Link { 
+  datum :: a,
+  next :: Link a
+} deriving (Show)
+
+nil :: Link a
+nil = Nil
+
+new :: a -> Link a -> Link a
+new d n = Link { datum = d, next = n}
+
+isNil :: Link a -> Bool
+isNil Nil = True
+isNil _ = False
+
+toList :: Link a -> [a]
+toList Nil = []
+toList (Link { datum = d, next = n }) =  d:toList n
+
+fromList :: [a] -> Link a
+fromList [] = Nil
+fromList (x:xs) = new x $ fromList xs
+
+reverseLinkedList :: Link a -> Link a
+reverseLinkedList = fromList . reverse . toList 
