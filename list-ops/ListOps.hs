9c0942cac49df433475024b81fd0f4150eeaa710
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-07 13:47:59 -0700

    First iteration list-ops

---
+++ list-ops/ListOps.hs
@@ -0,0 +1,40 @@
+module ListOps
+  ( length
+  , reverse
+  , map
+  , filter
+  , foldr
+  , foldl'
+  , (++)
+  , concat
+  ) where
+
+import Prelude hiding
+  ( length, reverse, map, filter, foldr, (++), concat )
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' _ acc [] = acc
+foldl' f acc (x:xs) = acc `seq` foldl' f (f acc x) xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ acc [] = acc
+foldr f acc (x:xs) = f x $ foldr f acc xs
+
+length :: [a] -> Int
+length = foldl' (\acc _ -> acc + 1) 0
+
+reverse :: [a] -> [a]
+reverse = foldl' (flip (:)) []
+
+map :: (a -> b) -> [a] -> [b]
+map f = foldr (\x acc -> (f x):acc) []
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter f = foldr (passes) []
+  where passes x acc = if f x then x:acc else acc
+
+(++) :: [a] -> [a] -> [a]
+xs ++ ys = foldr (:) ys xs
+
+concat :: [[a]] -> [a]
+concat = foldl' (++) []
