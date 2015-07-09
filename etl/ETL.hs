266cfa7cc1d25364af9a96084bbc2eac741e9f9c
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-09 11:38:50 -0700

    First iteration etl

---
+++ etl/ETL.hs
@@ -0,0 +1,14 @@
+module ETL (transform) where
+
+import qualified Data.Map as Map
+import Data.Char (toLower)
+
+type Old = Map.Map Int [String] 
+type New = Map.Map String Int
+
+transform :: Old -> New
+transform = Map.foldrWithKey (\k -> flip (addValKeysToMap k)) Map.empty
+
+addValKeysToMap :: Int -> New -> [String] -> New
+addValKeysToMap v = foldr (\k -> Map.insert (lower k) v)
+  where lower = map toLower
