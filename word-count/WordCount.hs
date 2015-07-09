547945096ce28043cc784a3b000244fe8bac8209
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-09 12:13:44 -0700

    Switched to fold'l in word-count

--- word-count/WordCount.hs
+++ word-count/WordCount.hs
@@ -1,10 +1,11 @@
 module WordCount (wordCount) where
 
 import qualified Data.Map as Map
+import Data.List (foldl')
 import Data.Char (isAlphaNum, toLower)
 
 wordCount :: String -> Map.Map String Int
-wordCount = foldr counter Map.empty . split (not . isAlphaNum) . map toLower
+wordCount = foldl' (flip counter) Map.empty . split (not . isAlphaNum) . map toLower
   where counter x = Map.insertWith (+) x 1
 
 split :: (Char -> Bool) -> String -> [String]
