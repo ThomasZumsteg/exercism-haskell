291b55452d858f4e3a02dcabbed8c189d7e53065
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-13 10:33:13 -0700

    Updated ETL to use strict folds

--- etl/ETL.hs
+++ etl/ETL.hs
@@ -1,14 +1,15 @@
 module ETL (transform) where
 
 import qualified Data.Map.Strict as Map
+import Data.List (foldl')
 import Data.Char (toLower)
 
 type Old = Map.Map Int [String] 
 type New = Map.Map String Int
 
 transform :: Old -> New
-transform = Map.foldrWithKey (\k -> flip (addValKeysToMap k)) Map.empty
+transform = Map.foldrWithKey' (\k -> flip (addValKeysToMap k)) Map.empty
 
 addValKeysToMap :: Int -> New -> [String] -> New
-addValKeysToMap v = foldr (\k -> Map.insert (lower k) v)
+addValKeysToMap v = foldl' (\m k -> Map.insert (lower k) v m)
   where lower = map toLower
