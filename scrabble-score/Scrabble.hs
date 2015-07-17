964296a9e1fe5c3e78eee67fe608bebcbde3d628
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-17 11:52:52 -0400

    Moved letter_score to it's own function

--- scrabble-score/Scrabble.hs
+++ scrabble-score/Scrabble.hs
@@ -3,19 +3,20 @@ module Scrabble (scoreLetter, scoreWord) where
 import Data.Char (toUpper)
 import qualified Data.Map as Map
 
+letter_scores :: Map.Map Char Int
+letter_scores = Map.fromList $ concatMap func [
+  (1, "AEIOULNRST"),
+  (2, "DG"),
+  (3, "BCMP"),
+  (4, "FHVWY"),
+  (5, "K"),
+  (8, "JX"),
+  (10, "QZ")]
+  where
+    func (score, letters) = map (\l' -> (l', score)) letters
+
 scoreLetter :: Char -> Int
 scoreLetter l = Map.findWithDefault 0 (toUpper l) letter_scores
-  where
-    letter_scores = Map.fromList $ concatMap func [
-      (1, "AEIOULNRST"),
-      (2, "DG"),
-      (3, "BCMP"),
-      (4, "FHVWY"),
-      (5, "K"),
-      (8, "JX"),
-      (10, "QZ")]
-      where
-        func (score, letters) = map (\l' -> (l', score)) letters
-
+    
 scoreWord :: String -> Int
 scoreWord = sum . map scoreLetter
