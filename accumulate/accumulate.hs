0cde2f3086009c22a97e8bbc85ab78b595f05d6c
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-24 12:32:49 -0400

    Updated accumulate to use cons operator and remove Ord type

--- accumulate/accumulate.hs
+++ accumulate/accumulate.hs
@@ -1,5 +1,5 @@
 module Accumulate (accumulate) where
 
-accumulate::(Ord a) => ( a -> b ) -> [a] -> [b]
+accumulate :: ( a -> b ) -> [a] -> [b]
 accumulate _ [] = []
-accumulate f (x:xs) = [ f x ] ++ accumulate f xs
+accumulate f (x:xs) =  f x : accumulate f xs
