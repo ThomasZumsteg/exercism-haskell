544509e2278edb69b45dbbb179d0dbfcc60679a7
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-25 14:18:28 -0400

    Make robot coords strict

--- robot-simulator/Robot.hs
+++ robot-simulator/Robot.hs
@@ -11,14 +11,22 @@ module Robot (
 
 import Data.List (foldl')
 
-data Bearing = North | East | South | West deriving (Show, Eq, Enum, Bounded)
-type Position = (Int, Int)
-data Robot = Robot { bearing :: Bearing, coordinates :: Position } deriving (Show, Eq)
+data Bearing = North | East | South | West 
+  deriving (Show, Eq, Enum, Bounded)
+
+data Robot = Robot { 
+    bearing :: !Bearing,
+    coord_x :: !Int,
+    coord_y :: !Int 
+  } deriving (Show, Eq)
 
 turn :: (Bounded a, Enum a) => Int -> a -> a
 turn s t =
   let max_t = fromEnum ( maxBound `asTypeOf` t ) + 1
   in toEnum (mod (s + fromEnum t) max_t)
+
+coordinates :: Robot -> (Int, Int)
+coordinates (Robot _ x y) = (x, y)
 
 turnLeft :: Bearing -> Bearing
 turnLeft = turn (-1)
@@ -26,21 +34,21 @@ turnLeft = turn (-1)
 turnRight :: Bearing -> Bearing
 turnRight = turn (1)
 
-mkRobot :: Bearing -> Position -> Robot
-mkRobot = Robot
+mkRobot :: Bearing -> (Int, Int) -> Robot
+mkRobot b (x, y) = Robot b x y
 
 simulate :: Robot -> String -> Robot
 simulate = foldl' commands
   where
-    commands r@(Robot b c) i = case i of
-      'R' -> Robot (turnRight b) c
-      'L' -> Robot (turnLeft b) c
+    commands r@(Robot b x y) i = case i of
+      'R' -> Robot (turnRight b) x y
+      'L' -> Robot (turnLeft b) x y
       'A' -> advance r
       i' -> error $ show i' ++ " is not a valid command" 
 
 advance :: Robot -> Robot
-advance (Robot b (x,y)) = case b of
-  East -> Robot b (x + 1, y)
-  West -> Robot b (x - 1, y)
-  North -> Robot b (x, y + 1)
-  South -> Robot b (x, y - 1)
+advance (Robot b x y) = case b of
+  East -> Robot b (x + 1) y
+  West -> Robot b (x - 1) y
+  North -> Robot b x (y + 1)
+  South -> Robot b x (y - 1)
