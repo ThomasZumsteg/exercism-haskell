75b0272d4bc61a7ec41fecea8c57bdfa021673c6
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-07-28 13:49:13 -0700

    Updated to use maybe function

--- binary-search-tree/BST.hs
+++ binary-search-tree/BST.hs
@@ -8,37 +8,32 @@ module BST (
     toList
   ) where
 
-import Data.Maybe (isNothing, fromJust)
 import Data.List (foldl')
 
 data Node a = Node { 
-    bstValue :: a,
     bstLeft  :: Maybe (Node a), 
-    bstRight :: Maybe (Node a)
+    bstRight :: Maybe (Node a),
+    bstValue :: a
   } deriving (Show, Eq)
 
 singleton :: a -> Node a
-singleton v = Node v Nothing Nothing
+singleton = Node Nothing Nothing
 
 insert :: (Ord a) => a -> Node a -> Node a
-insert v n@(Node value left right)
-  | v <= value = n { bstLeft = 
-    if isNothing left
-    then Just (singleton v)
-    else fmap (insert v) left }
-  | otherwise = n { bstRight = 
-    if isNothing right 
-    then Just (singleton v)
-    else fmap (insert v) right }
+insert v n@(Node left right value) =
+  if v <= value
+    then n { bstLeft  = add_node left }
+    else n { bstRight = add_node right }
+  where
+    add_node = Just . maybe (singleton v) (insert v)
 
 fromList :: (Ord a) => [a] -> Node a
 fromList (x:xs) = foldl' (flip insert) (singleton x) xs
 fromList [] = error "No empty lists"
 
 toList :: Node a -> [a]
-toList (Node value left right) = 
+toList (Node left right value) = 
   let
-    left_list  = if isNothing left  then [] else fromJust $ fmap toList left
-    right_list = if isNothing right then [] else fromJust $ fmap toList right
-  in
-    left_list ++ [value] ++ right_list
+    left_list  = maybe [] toList left
+    right_list = maybe [] toList right
+  in left_list ++ [value] ++ right_list
