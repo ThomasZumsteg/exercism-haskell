f7c144e015928d37e0feec6eb3670109b3f224ea
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-08-14 10:57:45 -0700

    Much faster primes

--- nth-prime/Prime.hs
+++ nth-prime/Prime.hs
@@ -1,17 +1,13 @@
-module Prime (nth, minus) where
+module Prime (nth) where
 
 nth :: Int -> Int
 nth = (primes !!) . pred
 
 primes :: [Int]
-primes = 2 : sieve [3,5..]
-  where
-    sieve (p:xs) = p : sieve (minus xs [p*p, p*(p+1)..])
+primes = 2 : filter isPrime [3,5..]
 
-minus :: Ord a => [a] -> [a] -> [a]
-minus [] _ = []
-minus xs [] = xs
-minus l1@(x:xs) l2@(y:ys)
-  | x > y = minus l1 ys
-  | x < y = x : minus xs l2
-  | otherwise = minus xs l2
+isPrime :: Int -> Bool
+isPrime n = not $ any (\x -> mod n x == 0) divisors
+  where
+    s = floor $ (sqrt :: Double -> Double) $ fromIntegral n
+    divisors = takeWhile (<= s) primes
