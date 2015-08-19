5fc6993634976af53f2ee840aadeedc506b11706
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-08-19 10:06:45 -0700

    Updated series to use tails

--- series/Series.hs
+++ series/Series.hs
@@ -1,12 +1,8 @@
 module Series (slices) where
 
 import Data.Char (digitToInt)
+import Data.List (tails)
 
 slices :: Int -> String -> [[Int]]
-slices n l
-  | length l < n = []
-  | otherwise = slice l : (slices n remainer)
-  where
-    slice = map digitToInt . take n
-    remainer = tail l
-    
+slices n l = map (map digitToInt . take n) sliceList
+  where sliceList = take (length l - n + 1) $ tails l
