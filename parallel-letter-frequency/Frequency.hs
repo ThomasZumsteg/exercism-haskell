d926ab2a710c4c84cc7c0eacbb3963d33da005bd
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-08-24 13:26:44 -0700

    Now with parallelism

--- parallel-letter-frequency/Frequency.hs
+++ parallel-letter-frequency/Frequency.hs
@@ -1,16 +1,15 @@
 module Frequency (frequency) where
 
 import qualified Data.Map.Strict as M
-import Data.Text (Text, foldl', toLower)
+import Data.Text (Text, foldl', toLower, filter)
 import Data.Char (isLetter)
+import Prelude hiding (filter)
+import Control.Parallel.Strategies
 
 frequency :: Int -> [Text] -> M.Map Char Int
-frequency _ = M.unionsWith (+) . map countLetters
+frequency workers = M.unionsWith (+) . map countLetters . flip using strategy
+  where strategy = parBuffer workers rseq
 
 countLetters :: Text -> M.Map Char Int
-countLetters = foldl' incrementMap M.empty . toLower
-  where
-    incrementMap m e
-      | e `M.member` m = M.adjust succ e m
-      | isLetter e = M.insert e 1 m
-      | otherwise = m
+countLetters = foldl' increment M.empty . toLower . filter isLetter
+  where increment m e = M.insertWith (+) e 1 m
