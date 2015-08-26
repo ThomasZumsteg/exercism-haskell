0862c879b179ef49bbea22700e081521df8d53a2
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-08-26 09:44:25 -0700

    Small improvments to sgf-parsing

--- sgf-parsing/Sgf.hs
+++ sgf-parsing/Sgf.hs
@@ -7,35 +7,36 @@ import Text.Parsec
 import Data.Char (isSpace)
 
 type SgfNode = Map.Map Text [Text]
+type Parser a = Parsec Text () a
 
 parseSgf :: Text -> Maybe (Tree (SgfNode))
 parseSgf = toMaybe . parse tree ""
   where
     toMaybe = either (const Nothing) Just
 
-tree :: Parsec Text () (Tree SgfNode)
+tree :: Parser (Tree SgfNode)
 tree = do
-  n <- char '(' *> many1 node 
-  trees <- many tree <* char ')'
-  return $ makeTree n trees
+    text <- char '(' *> many1 node
+    trees <- many tree <* char ')'
+    return $ makeTree text trees
 
-node :: Parsec Text () SgfNode
+node :: Parser SgfNode
 node = char ';' *> (Map.fromList <$> many properties)
 
-properties :: Parsec Text () (Text, [Text])
+properties :: Parser (Text, [Text])
 properties = do
   key <- many1 upper
   vals <- many1 val
   return (pack key, vals)
 
-val :: Parsec Text () Text
-val = char '[' *> worker [] False
+val :: Parser Text
+val = pack <$> (char '[' *> worker [] False)
   
-worker :: String -> Bool -> Parsec Text () Text
+worker :: String -> Bool -> Parser String
 worker acc bs = do
     c <- anyChar
     case c of
-        ']'  | not bs    -> return . pack . reverse $ acc
+        ']'  | not bs    -> return . reverse $ acc
         '\\' | not bs    -> worker acc True
         '\n' | bs        -> worker acc False
         _    | isSpace c -> worker (' ' : acc) False
@@ -44,4 +45,4 @@ worker acc bs = do
 makeTree :: [SgfNode] -> [(Tree SgfNode)] -> Tree SgfNode
 makeTree [] _ = error "Needs a root node"
 makeTree [n] trees = Node n trees
-makeTree (n:ns) trees = Node n [makeTree ns trees] 
+makeTree (n:ns) trees = Node n [makeTree ns trees]
